7f00b7d3e0a0d0ed2c9f1e57bff5163de4234c33
Brice Boucard <brice.boucard@gmail.com>
2014-08-03 14:15:18 +0200

    Initial commit. Google Analytics to Pico with few parameters.

---
+++ README.md
@@ -0,0 +1,28 @@
+Pico GAnalytics
+===============
+
+Adds automatically the Google Analytics tracking script to the head section of each page of the site.
+
+## Installation
+
+Place `pico_ganalytics.php` file into the `plugins` directory.
+
+## Configuration
+
+Inside the `config.php` file:
+
+````
+/* Google Analytics */
+// The Google Analytics ID for your site:
+// Admin --> Property --> Property Settings
+$config['GAnalytics']['trackingID'] = 'UA-53343030-1';
+/* Basic options */
+// Demographics and Interest Reports
+$config['GAnalytics']['demoint'] = true;
+//Enhanced link attribution
+$config['GAnalytics']['linkatt'] = true;
+````
+
+## TODO
+
+- adding personal tracking variables, goals... ?
---
+++ pico_ganalytics.php
@@ -0,0 +1,66 @@
+<?php 
+/**
+ * Pico Analytics
+ *
+ * Adds Google Analytics to your pages
+ *
+ * @author Brice Boucard
+ * @link https://github.com/bricebou/pico_SyntaxHighlighter
+ * @license http://bricebou.mit-license.org/
+ */
+
+class Pico_GAnalytics {
+
+	public function config_loaded(&$settings)
+	{	
+		if (isset($settings['GAnalytics']['trackingID']))
+        {
+            $this->GA_ID = $settings['GAnalytics']['trackingID'];
+        }
+        if (isset($settings['GAnalytics']['demoint'])) {
+        	$this->GA_demoint = $settings['GAnalytics']['demoint'];
+        }
+        if (isset($settings['GAnalytics']['linkatt'])) {
+        	$this->GA_linkatt = $settings['GAnalytics']['linkatt'];
+        }
+	}
+
+	public function build_ga()
+	{
+		if (isset($this->GA_ID) && $this->GA_ID != '') {
+			$gascript = '
+				<script>
+					(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
+			  		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				  	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+				  	})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');
+					
+					ga(\'create\', \''.$this->GA_ID.'\', \'auto\');
+			';
+
+			if (isset($this->GA_demoint) && $this->GA_demoint === true) {
+				$gascript .= '		ga(\'require\', \'displayfeatures\');
+				';
+			}
+			if (isset($this->GA_linkatt) && $this->GA_linkatt === true) {
+				$gascript .= '	ga(\'require\', \'linkid\', \'linkid.js\');
+				';
+			}
+			$gascript .= '	ga(\'send\', \'pageview\');
+				</script>
+			';
+
+			//$this->gascript = $gascript;
+			return $gascript;
+		}
+	}
+
+	public function after_render(&$output)
+	{
+		/*if(isset($this->GA_ID))
+		{*/	
+			$output = str_replace('</head>', PHP_EOL.$this->build_ga().'</head>', $output);
+		//}
+	}
+}
+?>
